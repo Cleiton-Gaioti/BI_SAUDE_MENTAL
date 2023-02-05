5b497d8b1f41bf9c0a265c5d1a824c5e65f9128f
gaioti <cleiton.s.gaioti@gmail.com>
2023-02-05 13:02:26 -0300

    subindo para mudar para o linux

--- code/CONNECTION.py
+++
@@ -1,6 +0,0 @@
-import sqlalchemy as sa
-
-
-def create_connection(server, database, username, password, port):
-    conn = f'postgresql+psycopg2://{username}:{password}@{server}:{port}/{database}'
-    return sa.create_engine(conn)
